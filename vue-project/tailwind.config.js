8080c7e0c3a53cfbcd0d91d33744c22993c58fcd
anna <anna.biausque@telecomnancy.net>
2024-11-06 00:06:40 +0100

    project start

---
+++ vue-project/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          background: "#FF9D3D"
+        }
+      }
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    }
+  },
+
+  plugins: [],
+}
+
