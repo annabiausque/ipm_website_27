c52c435f7907d73adc540a42fe84e8a7d44ab605
anna <anna.biausque@telecomnancy.net>
2024-11-06 02:22:34 +0100

    modif headbar

--- vue-project/src/components/SiteNavigation.vue
+++ vue-project/src/components/SiteNavigation.vue
@@ -1,25 +1,25 @@
 <template>
-    <header class="sticky top-0 bg-primary-background shadow-lg">
+    <header class="sticky top-0 bg-primary-background shadow-lg z-50">
       <nav
-        class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
+      class=" container flex flex-col sm:flex-row items-center gap-4 text-white py-6 "
       >
-        <RouterLink :to="{ name: 'home' }" class="flex-1">
-          <div class="flex items-center gap-3">
-            <img src="@/assets/logo.png" alt="Logo" class="h-8 w-8 sm:h-12 sm:w-12" />
-            <p class="text-2xl flex-1 text-center">Synapse</p>
-          </div>
-        </RouterLink>
-  
-        <div class="flex gap-3 flex-1 justify-end">
-          <RouterLink :to="{name : 'profile'}"  class="flex-1">
-          
-            <i class="fa-regular fa-user hover:text-primary-hover duration-150 cursor-pointer text-2xl"></i>
-          </RouterLink>
-          <i class="fa-solid fa-bars hover:text-primary-hover duration-150 cursor-pointer text-2xl"></i>
-   
-          </div>
-
+      <RouterLink :to="{ name: 'home' }" class="flex-1">
+        <div class="flex items-center gap-3">
+        <img src="@/assets/logo.png" alt="Logo" class="h-8 w-8 sm:h-12 sm:w-12" />
+        <p class="text-2xl flex-1 text-center">Synapse</p>
+        </div>
+      </RouterLink>
+    
+      <div class="flex gap-3 flex-1 justify-end">
+        <RouterLink :to="{name : 'profile'}"  class="flex-1">
         
+        <i class="fa-regular fa-user hover:text-primary-hover duration-150 cursor-pointer text-2xl"></i>
+        </RouterLink>
+        <i class="fa-solid fa-bars hover:text-primary-hover duration-150 cursor-pointer text-2xl"></i>
+     
+        </div>
+
+      
       </nav>
     </header>
   </template>
--- vue-project/src/views/MatchingView.vue
+++ vue-project/src/views/MatchingView.vue
@@ -1,9 +1,10 @@
+<script setup>
+import MatchingSlider from '../components/MatchingSlider.vue';
+</script>
+
 <template>
     <div>
 <MatchingSlider/>
     </div>
 </template>
 
-<script setup>
-import MatchingSlider from '../components/MatchingSlider.vue';
-</script>
