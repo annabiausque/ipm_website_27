a286eee4ee3b7a02b67fb9f29358b2cc9d9c08ac
anna <anna.biausque@telecomnancy.net>
2024-11-06 02:42:11 +0100

    sidebar

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class = "flex flex-col min-h-screen bg-primary-background">
+    <SideBar/>
     <SiteNavigation/>
+ 
  <RouterView/>
   </div>
 </template>
@@ -8,5 +10,6 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import SiteNavigation from './components/SiteNavigation.vue';
+import SideBar from './components/SideBar.vue';
 </script>
 
