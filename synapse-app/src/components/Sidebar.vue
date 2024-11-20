1f6e6f484af460fed745ef76cdc4708fc8a54d8b
Stanisław Pietryga <staspietryga@gmail.com>
2024-11-20 20:06:53 +0000

    More merge fixes

--- synapse-app/src/components/Sidebar.vue
+++ synapse-app/src/components/Sidebar.vue
@@ -22,9 +22,8 @@ const inactiveClass = ref(
 
     <div :class="isOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'"
       class="fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-gray-900 lg:translate-x-0 lg:static lg:inset-0">
-      <router-link to="/my-project" class="flex items-center justify-center mt-8">
+      <router-link to="/my-projects" class="flex items-center justify-center mt-8">
         <div class="flex items-center">
-          <img src="../assets/logo.png" class="w-16 h-16">
           <img src="../assets/logo.png" class="w-16 h-16">
           <span class="mx-2 text-2xl font-semibold text-white">Synapse</span>
         </div>
@@ -60,23 +59,16 @@ const inactiveClass = ref(
 
         <router-link class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
           :class="[$route.name === 'Tables' ? activeClass : inactiveClass]" to="/tables">
-        </router-link>
-
-        <router-link class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
-          :class="[$route.name === 'Tables' ? activeClass : inactiveClass]" to="/tables">
           <svg class="w-5 h-5" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M7 3C6.44772 3 6 3.44772 6 4C6 4.55228 6.44772 5 7 5H13C13.5523 5 14 4.55228 14 4C14 3.44772 13.5523 3 13 3H7Z"
               fill="currentColor" />
-            fill="currentColor" />
             <path
               d="M4 7C4 6.44772 4.44772 6 5 6H15C15.5523 6 16 6.44772 16 7C16 7.55228 15.5523 8 15 8H5C4.44772 8 4 7.55228 4 7Z"
               fill="currentColor" />
-            fill="currentColor" />
             <path
               d="M2 11C2 9.89543 2.89543 9 4 9H16C17.1046 9 18 9.89543 18 11V15C18 16.1046 17.1046 17 16 17H4C2.89543 17 2 16.1046 2 15V11Z"
               fill="currentColor" />
-            fill="currentColor" />
           </svg>
 
           <span class="mx-4">What's Synapse ?</span>
